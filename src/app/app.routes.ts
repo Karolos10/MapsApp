ed2ffbbdd5189f7e83df1fad2d8f51f1b0675f38
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-22 14:54:59 -0500

    Added responsive navbar component: - Implemented NavbarComponent with a responsive layout using DaisyUI and TailwindCSS. - Added dropdown menu in the navbar for navigation links (Homepage, Portfolio, About). - Included action buttons for search and notifications with indicators. - Styled the navbar with a shadow and 'cupcake' theme for a modern UI.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPageComponent } from './pages/fullscreen-map-page/fullscreen-map-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { HausesPageComponent } from './pages/hauses-page/hauses-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen-map',
+    component: FullscreenMapPageComponent,
+    title: 'Fullscreen Map',
+  },
+  {
+    path: 'markers',
+    component: MarkersPageComponent,
+    title: 'Markers',
+  },
+  {
+    path: 'hauses',
+    component: HausesPageComponent,
+    title: 'Available Properties',
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen-map',
+  },
+];
